d2d3160c3a6dbc28eef8b98b8f63c24cb51ea229
Jeonghyeok Lee <59694789+wjdgurrj@users.noreply.github.com>
2021-02-20 19:30:11 +0900

    Slave source

---
+++ Slave/DDUTIL.H
@@ -0,0 +1,24 @@
+/*==========================================================================
+ *
+ *  Copyright (C) 1995 Microsoft Corporation. All Rights Reserved.
+ *
+ *  File:       ddutil.cpp
+ *  Content:    Routines for loading bitmap and palettes from resources
+ *
+ ***************************************************************************/
+
+#include <tchar.h>
+
+#ifdef __cplusplus
+extern "C" {            /* Assume C declarations for C++ */
+#endif	/* __cplusplus */
+
+extern IDirectDrawSurface*  DDLoadBitmap(IDirectDraw *pdd, LPCTSTR szBitmap, int dx, int dy );
+extern HRESULT              DDCopyBitmap(IDirectDrawSurface *pdds, HBITMAP hbm, int x, int y, int dx, int dy);
+extern DWORD                DDColorMatch(IDirectDrawSurface *pdds, COLORREF rgb);
+extern HRESULT              DDSetColorKey(IDirectDrawSurface *pdds, COLORREF rgb);
+
+#ifdef __cplusplus
+}
+#endif	/* __cplusplus */
+

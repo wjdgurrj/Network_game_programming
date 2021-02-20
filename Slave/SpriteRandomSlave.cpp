99473c4b004ddc07af624183e8efb4b7f967ea77
Jeonghyeok Lee <59694789+wjdgurrj@users.noreply.github.com>
2021-02-20 19:32:10 +0900

    2player

---
+++ Slave/SpriteRandomSlave.cpp
@@ -0,0 +1,401 @@
+﻿#include <windows.h>
+#include <windowsx.h>
+#include <ddraw.h>
+#include <stdio.h>
+
+#include "ddutil.h"
+
+#include <dsound.h>
+#include "dsutil.h"
+
+
+#define _GetKeyState( vkey ) HIBYTE(GetAsyncKeyState( vkey ))
+#define _GetKeyPush( vkey )  LOBYTE(GetAsyncKeyState( vkey ))
+
+HWND MainHwnd;
+
+LPDIRECTDRAW         DirectOBJ;
+LPDIRECTDRAWSURFACE  RealScreen;
+LPDIRECTDRAWSURFACE  BackScreen;
+LPDIRECTDRAWSURFACE  SpriteImage;
+LPDIRECTDRAWSURFACE  BackGround;
+LPDIRECTDRAWSURFACE  SpriteImage2;
+LPDIRECTDRAWSURFACE  END;
+LPDIRECTDRAWSURFACE Player1W;
+LPDIRECTDRAWSURFACE Player2W;
+
+LPDIRECTDRAWCLIPPER	ClipScreen;
+
+int gFullScreen = 0, Click = 0;
+int gWidth = 640 * 2, gHeight = 480;
+int MouseX = 100, MouseY = gHeight / 2;
+
+////////////////////
+
+LPDIRECTSOUND       SoundOBJ = NULL;
+LPDIRECTSOUNDBUFFER SoundDSB = NULL;
+DSBUFFERDESC        DSB_desc;
+
+HSNDOBJ Sound[10];
+
+
+BOOL _InitDirectSound(void)
+{
+	if (DirectSoundCreate(NULL, &SoundOBJ, NULL) == DS_OK)
+	{
+		if (SoundOBJ->SetCooperativeLevel(MainHwnd, DSSCL_PRIORITY) != DS_OK) return FALSE;
+
+		memset(&DSB_desc, 0, sizeof(DSBUFFERDESC));
+		DSB_desc.dwSize = sizeof(DSBUFFERDESC);
+		DSB_desc.dwFlags = DSBCAPS_PRIMARYBUFFER | DSBCAPS_CTRLVOLUME | DSBCAPS_CTRLPAN;
+
+		if (SoundOBJ->CreateSoundBuffer(&DSB_desc, &SoundDSB, NULL) != DS_OK) return FALSE;
+		SoundDSB->SetVolume(DSBVOLUME_MAX); // DSBVOLUME_MIN
+		SoundDSB->SetPan(DSBPAN_RIGHT);
+		return TRUE;
+	}
+	return FALSE;
+}
+
+void _Play(int num)
+{
+	SndObjPlay(Sound[num], NULL);
+}
+
+////////////////////////
+
+
+BOOL Fail(HWND hwnd)
+{
+	ShowWindow(hwnd, SW_HIDE);
+	MessageBox(hwnd, "DIRECT X 초기화에 실패했습니다.", "게임 디자인", MB_OK);
+	DestroyWindow(hwnd);
+	return FALSE;
+}
+
+void _ReleaseAll(void)
+{
+	if (DirectOBJ != NULL)
+	{
+		if (RealScreen != NULL)
+		{
+			RealScreen->Release();
+			RealScreen = NULL;
+		}
+		if (SpriteImage != NULL)
+		{
+			SpriteImage->Release();
+			SpriteImage = NULL;
+		}
+		if (SpriteImage2 != NULL)
+		{
+			SpriteImage2->Release();
+			SpriteImage2 = NULL;
+		}
+		if (BackGround != NULL)
+		{
+			BackGround->Release();
+			BackGround = NULL;
+		}
+		DirectOBJ->Release();
+		DirectOBJ = NULL;
+	}
+}
+
+long FAR PASCAL WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int Step = 4;
+
+
+	switch (message)
+	{
+		/*
+				case    WM_MOUSEMOVE    :   MouseX = LOWORD(lParam);
+											MouseY = HIWORD(lParam);
+											break;
+		
+	case	WM_LBUTTONDOWN: 	Click = 1;
+		_Play(3);
+		break;
+		*/
+	case	WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+		case VK_F12:
+			PostMessage(hWnd, WM_CLOSE, 0, 0);
+			return 0;
+
+		case VK_LEFT:
+			MouseX -= Step;
+			return 0;
+
+		case VK_RIGHT:
+			MouseX += Step;
+			return 0;
+
+		case VK_UP:
+			MouseY -= Step;
+			return 0;
+
+		case VK_DOWN:
+			MouseY += Step;
+			return 0;
+
+		case VK_SPACE:
+			_Play(7);
+			Click = 1;
+			break;
+		}
+		break;
+
+	case    WM_DESTROY:  _ReleaseAll();
+						PostQuitMessage(0);
+						break;
+	}
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+BOOL _GameMode(HINSTANCE hInstance, int nCmdShow, int x, int y, int bpp)
+{
+	HRESULT result;
+	WNDCLASS wc;
+	DDSURFACEDESC ddsd;
+	DDSCAPS ddscaps;
+	LPDIRECTDRAW pdd;
+
+	wc.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+	wc.lpfnWndProc = WindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = GetStockBrush(BLACK_BRUSH);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = "TEST";
+	RegisterClass(&wc);
+
+
+	if (gFullScreen) {
+		if ((MainHwnd = CreateWindowEx(0, "TEST", NULL, WS_POPUP, 0, 0, GetSystemMetrics(SM_CXSCREEN),
+			GetSystemMetrics(SM_CYSCREEN), NULL, NULL, hInstance, NULL)) == NULL)
+			ExitProcess(1);
+	}
+	else {
+		if ((MainHwnd = CreateWindow("TEST", "2Player", WS_OVERLAPPEDWINDOW, 0, 0, x,
+			y, NULL, NULL, hInstance, NULL)) == NULL)
+			ExitProcess(1);
+		SetWindowPos(MainHwnd, NULL, 100, 100, x, y, SWP_NOZORDER);
+	}
+
+	SetFocus(MainHwnd);
+	ShowWindow(MainHwnd, nCmdShow);
+	UpdateWindow(MainHwnd);
+	//    ShowCursor( FALSE );
+
+	result = DirectDrawCreate(NULL, &pdd, NULL);
+	if (result != DD_OK) return Fail(MainHwnd);
+
+	result = pdd->QueryInterface(IID_IDirectDraw, (LPVOID *)&DirectOBJ);
+	if (result != DD_OK) return Fail(MainHwnd);
+
+
+	// 윈도우 핸들의 협력 단계를 설정한다.
+	if (gFullScreen) {
+		result = DirectOBJ->SetCooperativeLevel(MainHwnd, DDSCL_EXCLUSIVE | DDSCL_FULLSCREEN);
+		if (result != DD_OK) return Fail(MainHwnd);
+
+		result = DirectOBJ->SetDisplayMode(x, y, bpp);
+		if (result != DD_OK) return Fail(MainHwnd);
+
+		memset(&ddsd, 0, sizeof(ddsd));
+		ddsd.dwSize = sizeof(ddsd);
+		ddsd.dwFlags = DDSD_CAPS | DDSD_BACKBUFFERCOUNT;
+		ddsd.ddsCaps.dwCaps = DDSCAPS_PRIMARYSURFACE | DDSCAPS_FLIP | DDSCAPS_COMPLEX;
+		ddsd.dwBackBufferCount = 1;
+
+		result = DirectOBJ->CreateSurface(&ddsd, &RealScreen, NULL);
+		if (result != DD_OK) return Fail(MainHwnd);
+
+		memset(&ddscaps, 0, sizeof(ddscaps));
+		ddscaps.dwCaps = DDSCAPS_BACKBUFFER;
+		result = RealScreen->GetAttachedSurface(&ddscaps, &BackScreen);
+		if (result != DD_OK) return Fail(MainHwnd);
+	}
+	else {
+		result = DirectOBJ->SetCooperativeLevel(MainHwnd, DDSCL_NORMAL);
+		if (result != DD_OK) return Fail(MainHwnd);
+
+		memset(&ddsd, 0, sizeof(ddsd));
+		ddsd.dwSize = sizeof(ddsd);
+		ddsd.dwFlags = DDSD_CAPS;
+		ddsd.ddsCaps.dwCaps = DDSCAPS_PRIMARYSURFACE;
+		ddsd.dwBackBufferCount = 0;
+
+		result = DirectOBJ->CreateSurface(&ddsd, &RealScreen, NULL);
+		if (result != DD_OK) return Fail(MainHwnd);
+
+		memset(&ddsd, 0, sizeof(ddsd));
+		ddsd.dwSize = sizeof(ddsd);
+		ddsd.dwFlags = DDSD_CAPS | DDSD_HEIGHT | DDSD_WIDTH;
+		ddsd.ddsCaps.dwCaps = DDSCAPS_OFFSCREENPLAIN;
+		ddsd.dwWidth = x;
+		ddsd.dwHeight = y;
+		result = DirectOBJ->CreateSurface(&ddsd, &BackScreen, NULL);
+		if (result != DD_OK) return Fail(MainHwnd);
+
+		result = DirectOBJ->CreateClipper(0, &ClipScreen, NULL);
+		if (result != DD_OK) return Fail(MainHwnd);
+
+		result = ClipScreen->SetHWnd(0, MainHwnd);
+		if (result != DD_OK) return Fail(MainHwnd);
+
+		result = RealScreen->SetClipper(ClipScreen);
+		if (result != DD_OK) return Fail(MainHwnd);
+
+		SetWindowPos(MainHwnd, NULL, 100, 100, x, y, SWP_NOZORDER | SWP_NOACTIVATE);
+	}
+
+
+	return TRUE;
+}
+
+
+extern void CommInit(int argc, char **argv);
+extern void CommSend(char *sending);
+extern void CommRecv(char *recvData);
+
+void CALLBACK _GameProc(HWND hWnd, UINT message, UINT wParam, DWORD lParam)
+{
+	char sendData[200];
+	static int Frame = 0;
+
+	if (Click) {
+		sprintf_s(sendData, "1");
+		CommSend(sendData);
+		if (++Frame >= 5) {
+			Frame = 0;
+			Click = 0;
+		}
+	}
+
+
+	///////////////////////////////////////////////////////////////////////////////////
+}
+
+void _GameProcDraw(char *recvData)
+{
+	RECT BackRect = { 0, 0, 640, 480 };
+	RECT DispRect = { 0, 0, gWidth, gHeight };
+	RECT SpriteRect, dstRect, WinRect;
+	char sendData[200];
+	int type;
+	int winner = 0;
+	static int Rcount = 0;
+
+	if (Rcount == 0 ) { 
+		BackScreen->BltFast(0, 0, BackGround, &BackRect, DDBLTFAST_WAIT | DDBLTFAST_NOCOLORKEY);
+		BackScreen->BltFast(640, 0, BackGround, &BackRect, DDBLTFAST_WAIT | DDBLTFAST_NOCOLORKEY);
+	}
+
+
+	sscanf(recvData, "%d,%d,%d,%d,%d,%d,%d,%d,%d", &type, &dstRect.left, &dstRect.top, &dstRect.right, &dstRect.bottom, &SpriteRect.left, &SpriteRect.top, &SpriteRect.right, &SpriteRect.bottom);
+	if (type == 1)
+		BackScreen->BltFast(dstRect.left, dstRect.top, SpriteImage, &SpriteRect, DDBLTFAST_WAIT | DDBLTFAST_SRCCOLORKEY);
+	else if (type == 2)
+		BackScreen->BltFast(dstRect.left, dstRect.top, SpriteImage2, &SpriteRect, DDBLTFAST_WAIT | DDBLTFAST_SRCCOLORKEY);
+
+	else if (type == 11 && winner != 2)
+	{
+		winner = 1;
+		SpriteRect.left = 108;
+		SpriteRect.top = 115;
+		SpriteRect.right = 330;
+		SpriteRect.bottom = 159;
+
+		dstRect.left = 50;
+		dstRect.top = 300;
+		dstRect.right = dstRect.left + 222;
+		dstRect.bottom = dstRect.top + 44;
+
+		BackScreen->BltFast(0, 0, END, &BackRect, DDBLTFAST_WAIT | DDBLTFAST_NOCOLORKEY);
+		BackScreen->BltFast(640, 0, Player1W, &BackRect, DDBLTFAST_WAIT | DDBLTFAST_NOCOLORKEY);
+
+
+	}
+	else if (type == 12 && winner != 1)
+	{
+		winner = 2;
+		SpriteRect.left = 108;
+		SpriteRect.top = 115;
+		SpriteRect.right = 330;
+		SpriteRect.bottom = 159;
+
+		dstRect.left = 50;
+		dstRect.top = 300;
+		dstRect.right = dstRect.left + 222;
+		dstRect.bottom = dstRect.top + 44;
+
+		BackScreen->BltFast(0, 0, END, &BackRect, DDBLTFAST_WAIT | DDBLTFAST_NOCOLORKEY);
+		BackScreen->BltFast(640, 0, Player2W, &BackRect, DDBLTFAST_WAIT | DDBLTFAST_NOCOLORKEY);
+
+	}
+	
+	GetWindowRect(MainHwnd, &WinRect);
+	RealScreen->Blt(&WinRect, BackScreen, &DispRect, DDBLT_WAIT, NULL);
+	Rcount++;
+	if (Click) //2Player 움직이기
+	{
+		sprintf_s(sendData, "10"); //master에 10 전송
+		CommSend(sendData);
+	}
+
+}
+
+
+
+int PASCAL WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	MSG msg;
+
+	if (!_GameMode(hInstance, nCmdShow, gWidth, gHeight, 32)) return FALSE;
+	SpriteImage = DDLoadBitmap(DirectOBJ, "CUTEBABY.BMP", 0, 0);
+	SpriteImage2 = DDLoadBitmap(DirectOBJ, "CUTEBABYY.BMP", 0, 0);
+	BackGround = DDLoadBitmap(DirectOBJ, "BACKGROUND.BMP", 0, 0);
+	END = DDLoadBitmap(DirectOBJ, "END.BMP", 0, 0);
+	Player1W = DDLoadBitmap(DirectOBJ, "Player1W.BMP", 0, 0);
+	Player2W = DDLoadBitmap(DirectOBJ, "Player2W.BMP", 0, 0);
+
+	DDSetColorKey(SpriteImage, RGB(0, 0, 0));
+	DDSetColorKey(SpriteImage2, RGB(0, 0, 0));
+	DDSetColorKey(END, RGB(0, 0, 0));
+	
+	SetTimer(MainHwnd, 1, 30, _GameProc);
+
+	CommInit(NULL, NULL);
+
+	if (_InitDirectSound())
+	{
+		Sound[0] = SndObjCreate(SoundOBJ, "BACKSOUND.WAV", 1);
+		Sound[7] = SndObjCreate(SoundOBJ, "UPDOWN.WAV", 2);
+
+		       SndObjPlay( Sound[0], DSBPLAY_LOOPING );
+	}
+
+	while (!_GetKeyState(VK_ESCAPE))
+	{
+
+		if (PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE))
+		{
+			if (!GetMessage(&msg, NULL, 0, 0)) return msg.wParam;
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		//        else _GameProc();
+	}
+	DestroyWindow(MainHwnd);
+
+	return TRUE;
+}
---
+++ Slave/tcpClientPacket.cpp
@@ -0,0 +1,293 @@
+#include <stdio.h>
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+
+#include "wtpipv6.h"
+#include "wspiapi.h"
+
+#include "Packet.h"
+
+
+//**********************************************************************
+void usage(char *progname)
+{
+    fprintf(stderr, "usage: %s [-n name] [-p port] \n", progname);
+    fprintf(stderr, "   -n name    Host name to resolve, [127.0.0.1] \n");
+    fprintf(stderr, "   -p port    Port number to resolve, [8600] \n");
+  
+    ExitProcess(-1);
+}
+
+
+int resolveAddr(int argc, char **argv, char *serverName, char *serverPort)
+{
+	int count, rc, i;
+
+	serverName[0]=0;
+	serverPort[0]=0;
+
+    for(i=1; i < argc ;i++)
+    {
+        if ((argv[i][0] != '-') && (argv[i][0] != '/') && (strlen(argv[i]) < 2))
+            usage(argv[0]);
+
+        switch (tolower(argv[i][1]))
+        {
+
+            case 'n':       // name to resolve
+                if (i+1 >= argc)
+                    usage(argv[0]);
+                strcpy(serverName, argv[++i]);
+                break;
+
+            case 'p':       // port/service to resolve
+                if (i+1 >= argc)
+                    usage(argv[0]);
+                strcpy(serverPort, argv[++i]);
+                break;
+
+
+            default:
+                usage(argv[0]);
+                break;
+        }
+    }
+	if(serverName[0]==0)
+		strcpy(serverName, "127.0.0.1");
+	if(serverPort[0]==0)
+		strcpy(serverPort, "8600");
+
+	printf("** Resolve Address %s:%s \n", serverName, serverPort);
+
+
+	//******************* remoteHost = gethostbyname(host_name);
+
+	struct hostent *remoteHost;
+
+	remoteHost = gethostbyname(serverName);	// ns.cau.ac.kr
+	printf(">> gethostbyname Official name: %s", remoteHost->h_name);
+
+    struct in_addr addr;
+
+	addr.s_addr = *(u_long *) remoteHost->h_addr_list[0];
+	printf("-(%s) \n", inet_ntoa(addr));
+
+
+	if(!isalpha(serverName[0]))
+	{
+		struct in_addr addr = { 0 };
+		addr.s_addr = inet_addr(serverName); 	// 127.0.0.1
+		remoteHost = gethostbyaddr((char *) &addr, 4, AF_INET);
+		printf(">> gethostbyaddr Official name: %s\n", remoteHost->h_name);
+	}
+
+	//*****************************************************************************
+
+    // Resolve the name/address - first assume that the name might be a string
+    // literal address
+    struct addrinfo  hints, *res=NULL, *ptr;
+
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_flags = AI_NUMERICHOST;
+    hints.ai_family    = AF_INET;
+    hints.ai_socktype  = SOCK_STREAM;
+    hints.ai_protocol  = IPPROTO_TCP;
+
+    rc = getaddrinfo( serverName, serverPort, &hints, &res);
+    if (rc != 0)
+    {
+        if (rc == WSAHOST_NOT_FOUND)
+        {
+            hints.ai_flags = AI_CANONNAME;
+			printf("** AI_CANONNAME\n");
+            rc = getaddrinfo( serverName, serverPort, &hints, &res );
+            if (rc != 0)
+            {
+                fprintf(stderr, "getaddrinfo failed: %d\n", rc);
+                return -1;
+            }
+        }
+        else
+        {
+            fprintf(stderr, "getaddrinfo failed: %d\n", rc);
+            return -1;
+        }
+    }
+
+    // Count how many addresses were returned
+    count = 0;
+    ptr   = res;
+    while (ptr)
+    {
+        count++;
+        ptr = ptr->ai_next;
+    }
+    printf("** Hostname '%s' resolved to %d addresses\n", serverName, count);
+
+
+    //Do a reverse lookup on each of the resolved address
+    char numerichost[NI_MAXHOST];
+    
+	rc = getnameinfo( res->ai_addr, res->ai_addrlen, numerichost, NI_MAXHOST, NULL, 0, NI_NUMERICHOST ); // NI_NAMEREQD, NI_NUMERICHOST
+    if (rc != 0)
+    {
+        fprintf(stderr, "getnameinfo failed: %d\n", rc);
+        return -1;
+    }
+    strcpy( serverName, numerichost);
+
+    printf(">> Numeric address resolved: %s:%s\n", serverName, serverPort);
+ 
+    // Free up the results from getaddrinfo
+    freeaddrinfo(res);
+
+	return 1;
+}
+//************************************************************************
+
+void CommRecv(char *recvData);
+
+DWORD WINAPI NetReceive(LPVOID socketConnect)
+{
+	char recvBuffer[127];
+	int  RecvBytes;
+
+	Packet	recvPacket;
+	char	receiveBuffer[PACKETBUFFERSIZE];
+	int		receivedPacketSize=0;
+
+
+	while(1){
+
+		///////////////////////////////////////////////////////
+		int bufSize = PACKETBUFFERSIZE - receivedPacketSize;
+
+		if((RecvBytes=recv((SOCKET)socketConnect, &(receiveBuffer[receivedPacketSize]), bufSize, 0))<1){
+			::shutdown( (SOCKET) socketConnect, SD_BOTH );
+			::closesocket( (SOCKET) socketConnect);
+			socketConnect= (LPVOID) INVALID_SOCKET;
+			break;
+		}
+		receivedPacketSize += RecvBytes;
+
+		while( receivedPacketSize > 0 )	// parsing Packet Length
+		{
+			recvPacket.copyToBuffer( receiveBuffer, receivedPacketSize);
+			int packetlength=( int )recvPacket.getPacketSize();
+
+			if( receivedPacketSize >= packetlength)
+			{
+				//  Parsing, main routine 
+				recvPacket.readData(recvBuffer, recvPacket.getDataFieldSize() );
+				printf( "(%d Bytes, ID=%d) %s\n", recvPacket.getDataFieldSize(), recvPacket.id(), recvBuffer );
+				CommRecv(recvBuffer);
+
+				receivedPacketSize -= packetlength;
+				if( receivedPacketSize > 0 )
+				{
+					::CopyMemory( recvBuffer, ( receiveBuffer + recvPacket.getPacketSize() ), receivedPacketSize );
+					::CopyMemory( receiveBuffer, recvBuffer, receivedPacketSize );
+				}
+			}
+			else{// if(recvPacket.id()==0){
+				printf( "(%d Bytes, ID=%d) %s\n", recvPacket.getDataFieldSize(), recvPacket.id(), receiveBuffer );
+//				receivedPacketSize=0;
+				break;
+			}
+		}
+
+		///////////////////////////////////////////////////////
+
+	}
+	
+	return NULL;
+}
+
+
+
+SOCKET socketConnect=INVALID_SOCKET;
+
+void CommInit(int argc, char **argv)
+{
+	WSADATA wsaData;
+	struct sockaddr_in serverAddr;
+	HANDLE handleThread;
+
+
+	::WSAStartup( 0x202, &wsaData );
+
+
+	socketConnect = ::socket( AF_INET, SOCK_STREAM, IPPROTO_TCP );
+	if( socketConnect == INVALID_SOCKET )
+	{
+		printf( "Cannot create socket !!\n");
+	}
+
+
+	//******************************* Address and port resolve
+	char serverName[120], serverPort[120];
+
+	if(resolveAddr(argc, argv, serverName, serverPort)<1){
+		printf("*** Unable to resolve server name !\n");
+	    ExitProcess(-1);
+	}
+
+	//  접속할 서버의 정보를 설정한다.
+	::memset( &serverAddr, 0, sizeof( serverAddr ) );
+	serverAddr.sin_family		= AF_INET;
+	serverAddr.sin_addr.s_addr	= ::inet_addr( serverName );
+	serverAddr.sin_port			= ::htons( atoi(serverPort) );
+
+	//********************************************************
+
+
+	if( socketConnect != INVALID_SOCKET){
+		if(::connect( socketConnect, ( struct sockaddr* )&serverAddr, sizeof( serverAddr ) ) == SOCKET_ERROR )	{
+//			printf( "Cannot connect to server !!\n");
+			socketConnect = INVALID_SOCKET;
+			ExitProcess(-1);
+			}
+		else{
+			// create thread for receive
+			handleThread =CreateThread(NULL, 0, NetReceive, (void *) socketConnect, THREAD_PRIORITY_NORMAL, NULL);
+		}
+	}
+
+}
+
+
+
+void CommSend(char *sending) 
+{
+	char sendData[200];
+	Packet sendPacket;
+	int sentBytes;
+
+	if (socketConnect == INVALID_SOCKET)
+		return;
+
+	sprintf(sendData, "%s", sending);
+
+	sendPacket.clear();
+	sendPacket.id(1001);
+	sendPacket.writeData(sendData, strlen(sendData) + 1);
+
+	sentBytes = ::send(socketConnect, sendPacket.getPacketBuffer(), sendPacket.getPacketSize(), 0);
+
+	if (sentBytes < 0) {
+		::shutdown(socketConnect, SD_BOTH);
+		::closesocket(socketConnect);
+		socketConnect = INVALID_SOCKET;
+	}
+}
+
+extern void _GameProcDraw(char *recvData);
+
+void CommRecv(char *recvData)
+{
+	_GameProcDraw(recvData);
+}
+
+
+
